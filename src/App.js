eed4008a84b16aab3003b7f6a3a2eb8ef5bca7cc
FathimaNajafK <fathimanajafk@gmail.com>
2024-03-18 03:11:10 -0700

    create pages and routing

--- src/App.js
+++ src/App.js
@@ -1,114 +1,20 @@
+
 import React from 'react';
-import { Table } from 'antd';
+import { Routes, Route } from 'react-router-dom';
+import AddTransaction from './pages/AddTransaction';
+import Home from './pages/Home';
+import Transaction from './pages/Transaction';
 
 function App() {
-  const columns = [
-    {
-      title: 'Stock Name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'Live Price',
-      dataIndex: 'chinese',
-      sorter: {
-        compare: (a, b) => a.chinese - b.chinese,
-        multiple: 3,
-      },
-    },
-    {
-      title: 'Math Score',
-      dataIndex: 'math',
-      sorter: {
-        compare: (a, b) => a.math - b.math,
-        multiple: 2,
-      },
-    },
-    {
-      title: 'English Score',
-      dataIndex: 'english',
-      sorter: {
-        compare: (a, b) => a.english - b.english,
-        multiple: 1,
-      },
-    },
-    {
-      title: 'Quantity',
-      dataIndex: 'quantity',
-      sorter: {
-        compare: (a, b) => a.quantity - b.quantity,
-        multiple: 0,
-      },
-
-    }
-  ];
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      chinese: 98,
-      math: 60,
-      english: 70,
-      quantity: 2,
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      chinese: 98,
-      math: 66,
-      english: 89,
-      quantity: 7,
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      chinese: 98,
-      math: 90,
-      english: 70,
-      quantity: 9,
-    },
-    {
-      key: '4',
-      name: 'Jim Red',
-      chinese: 88,
-      math: 99,
-      english: 89,
-      quantity: 5,
-    },
-
-  ];
-  const onChange = (pagination, filters, sorter, extra) => {
-    console.log('params', pagination, filters, sorter, extra);
-  };
-
   return (
-    <>
-      <div class="container">
-        <div class="flex-display">
-          <h2 >My Account</h2>
-          <input />
-          <h4 ><span>Add</span> | <span>MANAGE</span></h4>
-          <h4 >Refresh</h4>
-        </div>
-        <div class='flex-display'>
-          <h4 > + Add Transaction</h4>
-          <h4 > My Feed</h4>
-          <h4 > Icon</h4>
-          <h4 > Print</h4>
-        </div>
-      </div>
-      <div className='container'>
-      <div className='flex-display'>
-        <h3 >FILTER</h3>
-        <h4 > <span>All</span> | <span>Gainers</span> | <span>Losers</span></h4>
-      </div>
-      <div className='flex-display'>
-        <h3 >Group By</h3>
-        <h4><input type='radio'/> None | <input type='radio'/> None | <input type='radio'/> None</h4>
-      </div>
-      </div>
-      <Table columns={columns} dataSource={data} onChange={onChange} />
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route element={<AddTransaction />} path='/add-transaction' />
+        <Route element={<Home />} path='/home' />
+        <Route element={<Transaction />} path='/transaction' />
+      </Routes>
+    </div>
+  );
 }
-;
+
 export default App;
